2247e46b03a8f77f84f008d988fe87740208d12a
資訊部 袁華建 <I356@nttp3.ths.com.tw>
2024-07-04 11:00:58 +0800

    misc: copy files

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# linebot
-linebot test
+# Fastapi Example for LINE Bot
+
+## Prerequisite
+
+- Python 3.x
+- Docker
+
+## Development
+
+```
+pip install -r requirements.txt
+python main.py
+```
+
+#### OR
+
+```shell
+docker-compose up
+```
+
+## Testing
+
+```shell
+curl http://localhost:5000/
+```
+
+## LICENSE
+
+MIT
---
+++ routers/webhooks.py
@@ -0,0 +1,53 @@
+import os
+from typing import List, Optional
+
+from fastapi import APIRouter, HTTPException, Header, Request
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import TextMessage, MessageEvent, TextSendMessage, StickerMessage, \
+    StickerSendMessage
+from pydantic import BaseModel
+
+line_bot_api = LineBotApi('nMNiuEQTihA4MwQ+jmOuuVEBzTlkaN1vy9wKNJ0HB1wwZwqmqS/eo8U5ULj0HuhyrPdiPIi2pBwZwn0kcR6Fj1na02k1MWyR3ZRFijOi+4iuBmdrLZPw/1JkjXj1tdZxamhkX/h7shKF+uZUNsfeVwdB04t89/1O/w1cDnyilFU=')
+handler = WebhookHandler('6025c873a456c88e7401e7ec4da89d23')
+
+router = APIRouter(
+    prefix="/webhooks",
+    tags=["chatbot"],
+    responses={404: {"description": "Not found"}},
+)
+
+
+class Line(BaseModel):
+    destination: str
+    events: List[Optional[None]]
+
+
+@router.post("/line")
+async def callback(request: Request, x_line_signature: str = Header(None)):
+    body = await request.body()
+    try:
+        handler.handle(body.decode("utf-8"), x_line_signature)
+    except InvalidSignatureError:
+        raise HTTPException(status_code=400, detail="chatbot handle body error.")
+    return 'OK'
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def message_text(event):
+    print("!!!!!!!!!!!!!!!!!!!!!!")
+    print(event)
+    print("!!!!!!!!!!!!!!!!!!!!!!")
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text)
+    )
+
+
+@handler.add(MessageEvent, message=StickerMessage)
+def sticker_text(event):
+    # Judge condition
+    line_bot_api.reply_message(
+        event.reply_token,
+        StickerSendMessage(package_id='6136', sticker_id='10551379')
+    )
